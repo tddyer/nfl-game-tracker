45ea47a94306cf0a796b4694cab7e7ebea144ad9
Tanner <tdyer@hawk.iit.edu>
2020-09-19 16:41:40 -0500

    incorporated dynamic widget selection based upon phone platform

--- lib/matchups_screen.dart
+++ lib/matchups_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'matchup_data.dart';
+import 'dart:io' show Platform;
 
 class MatchupsScreen extends StatefulWidget {
   @override
@@ -10,10 +12,10 @@ class _MatchupsScreenState extends State<MatchupsScreen> {
 
   String selectedLeague = 'NFL';
 
-  List<DropdownMenuItem> getLeagues() {
-
+  // generates Android DropdownButton for android devices
+  DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropDownItems = [];
-    
+
     for (String league in leagues) {
       var menuItem = DropdownMenuItem(
         child: Text(league),
@@ -21,7 +23,39 @@ class _MatchupsScreenState extends State<MatchupsScreen> {
       );
       dropDownItems.add(menuItem);
     }
-    return dropDownItems;
+
+    return DropdownButton<String>(
+      value: selectedLeague,
+      items: dropDownItems,
+      onChanged: (value) {
+        setState(() {
+          selectedLeague = value;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker iOSPicker() {
+
+    List<Text> pickerItems = [];
+
+    for (String league in leagues) {
+      pickerItems.add(Text(league));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: pickerItems,
+    );
+  }
+
+
+  List<Text> getPickerItems() {
+    
   }
 
   @override
@@ -72,18 +106,11 @@ class _MatchupsScreenState extends State<MatchupsScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton<String>(
-              value: selectedLeague,
-              items: getLeagues(),
-              onChanged: (value) {
-                setState(() {
-                  selectedLeague = value;
-                });
-              },
-            ),
+            child: Platform.isIOS ? iOSPicker() : androidDropdown(), 
           ),
         ],
       ),
     );
   }
 }
+
